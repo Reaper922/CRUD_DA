c5d6d3f95ef0684557d57536c36309abc1e4be01
Dennis Ammen <dennis.ammen@outlook.com>
2022-08-22 21:50:08 +0200

    Function add contacts

--- script.js
+++ script.js
@@ -5,16 +5,32 @@ let phoneNumbers = ['0157123345678', '015798765432'];
 function render() {
     let content = document.getElementById('content');
     content.innerHTML = '';
-    content.innerHTML += `<h1>My Contacts</h1>`;
+    content.innerHTML += `
+        <h1>My Contacts</h1>
+        <div>
+            <input placeholder="Name" id="name">
+            <input placeholder="Telefon" id="phone">
+            <button onclick="addContact()">Hinzufügen</button>
+        </div>`;
 
     for (let i = 0; i < names.length; i++) {
         const name = names[i];
         const phoneNumber = phoneNumbers[i];
 
         content.innerHTML += `
-        <div class="card">
-            <b>Name: </b> ${name} <br>
-            <b>Telefon: </b> ${phoneNumber} <br>
-        </div>`;
+            <div class="card">
+                <b>Name: </b> ${name} <br>
+                <b>Telefon: </b> ${phoneNumber} <br>
+            </div>`;
     }
+}
+
+function addContact() {
+    let name = document.getElementById('name');
+    let phone = document.getElementById('phone');
+
+    names.push(name.value);
+    phoneNumbers.push(phone.value);
+
+    render();
 }
